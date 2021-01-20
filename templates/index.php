692ba03151ea1e27f731ef3b7da3b3353ba5c1cf
waldos09 <legos567@gmail.com>
2021-01-20 15:11:30 -0600

    v.5

---
+++ templates/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mental Analitica</title>
+    <link rel="stylesheet" href="{{ url_for('static', filename='style.css' ) }}">
+</head>
+
+<body>
+    <div class="banner ">
+        <section>
+            <h2>
+                <p>Ingresa el correo del usuario que busques </p>
+            </h2>
+            <form action="/result " method="post ">
+                <input type="text " name="email " placeholder="Correo">
+                <button type="submit ">BUSCAR</button>
+            </form>
+        </section>
+        <div class="clouds ">
+            <img src="{{ url_for('static', filename='img/cloud1.png') }} " style="--i:1; ">
+            <img src="{{ url_for('static', filename='img/cloud2.png') }} " style="--i:2; ">
+            <img src="{{ url_for('static', filename='img/cloud3.png') }} " style="--i:3; ">
+            <img src="{{ url_for('static', filename='img/cloud4.png') }} " style="--i:4; ">
+            <img src="{{ url_for('static', filename='img/cloud5.png') }} " style="--i:5; ">
+        </div>
+    </div>
+
+
+
+
+</body>
+
+</html>
